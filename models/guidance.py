691e411c4a95bbc8da1a0f04d65e6ff3cd2e7531
Tristan Lilienthal <tristan.lilienthal@tristan.lilienthal@quantom.de>
2024-12-17 13:11:48 +0100

    guidace updated

--- models/guidance.py
+++ models/guidance.py
@@ -23,7 +23,7 @@ class Guidance:
         "3. Set the pressure regulator to 0 mbar",
         "4. Connect the pressure reducer to the nitrogen supply  and the nitrogen inlet of the test system",
         "5. Open the gas supply to the nitrogen supply",
-        "6. Slowly set the pressure reducer to the inlet pressure of 1 bar",
+        "6. Slowly set the pressure reducer to the inlet pressure of 500 mbar",
         "7. Open the inlet valve of the test system",
         "8. Slowly set the pressure regulator  to the test pressure of 200 mbar"
         ]
